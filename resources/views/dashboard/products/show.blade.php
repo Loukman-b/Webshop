be8ad4154133b5e25eb5705d2ceebeb8b2c276ad
Loukman Bouhtala <B.loukman2004@gmail.com>
2024-11-18 13:34:40 +0100

    Verandering in de uiterlijk van de pagina en hoe de product op het scherm komt

--- resources/views/dashboard/products/show.blade.php
+++ resources/views/dashboard/products/show.blade.php
@@ -1,28 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h1>Dashboard</h1>
-        <h2>Product Detailpagina</h2>
-        <hr>
-        
-        <div class="card-image" style="height: 200px; overflow: hidden;">
-            <img src="{{ asset('storage/' . $product->image) }}" alt="Product Image">
-        </div>        
-        <h3>Naam:{{ $product->name }}</h3>
-        <p><strong>Merk:</strong> {{ $product->merk }}</p>
-        <p><strong>Prijs:</strong> €{{ $product->price }}</p>
-        <p><strong>Beschrijving:</strong> {{ $product->description }}</p>
+<div class="container">
+    <h1 class="my-4 text-center">Product Details</h1>
 
-        
-        <div class="buttons d-flex mt-4">
-            <a href="{{ route('products.edit', $product->id) }}" class="btn btn-info me-2">Aanpassen</a>
-            
-            <form action="{{ route('products.destroy', $product->id) }}" method="post" onsubmit="return confirm('Weet je zeker dat je dit product wilt verwijderen?');">
-                @csrf
-                @method('DELETE')
-                <input type="submit" value="Verwijderen" class="btn btn-danger">
-            </form>
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div class="col">
+            <div class="card h-100 shadow-sm border-0">
+                <div class="card-image" style="height: 200px; overflow: hidden;">
+                    <img src="{{ asset('storage/' . $product->image) }}" alt="Product Image" class="w-100 h-100 object-fit-cover">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">{{ $product->name }}</h5>
+                    <p class="text-muted"><strong>Merk:</strong> {{ $product->merk }}</p>
+                    <p><strong>Prijs:</strong> €{{ $product->price }}</p>
+                    <p class="card-text"><strong>Beschrijving:</strong> {{ $product->description }}</p>
+                </div>
+                <div class="card-footer d-flex justify-content-between">
+                    <a href="{{ route('products.edit', $product->id) }}" class="btn btn-primary">Pas product aan</a>
+                    <form action="{{ route('products.destroy', $product->id) }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <input type="submit" value="Verwijderen" class="btn btn-danger">
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
+</div>
 @endsection
